8ae242b3a9b4ea16da38500c5e9776619cbf0f49
asatia07 <asatia07@gmail.com>
2015-09-30 16:48:02 +0530

    import model

--- indeed/views.py
+++ indeed/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 from lib import indeed_api
+from models import Trade
 
 def home(request):
     trades = Trade.objects.all()
